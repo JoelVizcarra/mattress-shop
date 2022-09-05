c03bc2cbc70c96467333d8f3bd17d316d2792408
Joel Vizcarra <joel@Joels-MacBook-Pro.local>
2022-09-05 12:08:29 -0500

    minor fix

--- src/services.js
+++ src/services.js
@@ -1,8 +1,8 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
-import { mattresses } from "../data/mattresses.json";
+import { mattresses } from '../data/mattresses.json';
 
-const TIME_OUT = 1500;
+const TIME_OUT = 1000;
 
 const data = Object.values(mattresses).map((item) => ({
   ...item,
